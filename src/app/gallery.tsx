d5e0375387281a0d41276b340f0f2d143c8a8946
smdhnz <strikerfmy1208@gmail.com>
2024-01-31 12:58:43 +0900

    add animation

--- src/app/gallery.tsx
+++ src/app/gallery.tsx
@@ -39,24 +39,26 @@ export function Gallery({ initialPosts }: Props) {
   }, [inView]);
 
   return (
-    <div className="flex flex-col items-center w-full gap-4">
-      {posts.map(({ id, attachments, content }) => (
-        <div key={id} className="w-[300px]">
-          <AspectRatio
-            ratio={1 / 1}
-            className="rounded-xl border overflow-hidden flex items-center justify-center"
-          >
-            <Image
-              src={attachments[0]?.url ?? ""}
-              alt={attachments[0]?.filename ?? ""}
-              width={attachments[0]?.width}
-              height={attachments[0]?.height}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
-          </AspectRatio>
-        </div>
-      ))}
-      {!last && <div ref={ref}>Loading...</div>}
+    <div>
+      <div className="flex flex-col items-center w-full gap-4">
+        {posts.map(({ id, attachments, content }) => (
+          <div key={id} className="w-[300px]">
+            <AspectRatio
+              ratio={1 / 1}
+              className="rounded-xl border overflow-hidden flex items-center justify-center animate-slide-top"
+            >
+              <Image
+                src={attachments[0]?.url ?? ""}
+                alt={attachments[0]?.filename ?? ""}
+                width={attachments[0]?.width}
+                height={attachments[0]?.height}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </AspectRatio>
+          </div>
+        ))}
+      </div>
+      {!last && <div ref={ref} />}
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -67,14 +67,26 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "slide-top": {
+          from: {
+            transform: "translateY(50px)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-top":
+          "slide-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
